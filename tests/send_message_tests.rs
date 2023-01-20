d9a8313982d2180182d33edd05b4d8f7b8dfbbe2
Juantamayo26 <juancyepest@gmail.com>
2023-01-20 01:35:37 -0500

    fixed test

--- tests/send_message_tests.rs
+++ tests/send_message_tests.rs
@@ -10,8 +10,10 @@ async fn send_text_message_works() -> Result<(), WhatsappError> {
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
+    let phone_number_id =
+        std::env::var("PHONE_NUMBER_ID").expect("Missing environment variable PHONE_NUMBER_ID");
     let message = Message::from_text(&to, "test message");
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
@@ -24,11 +26,13 @@ async fn send_message_template_works() -> Result<(), WhatsappError> {
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
+    let phone_number_id =
+        std::env::var("PHONE_NUMBER_ID").expect("Missing environment variable PHONE_NUMBER_ID");
     let template_name = "hello_world";
     let language = "en_US";
     let template = Template::new(template_name, language);
     let message = Message::from_template(&to, template);
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
@@ -41,13 +45,15 @@ async fn send_message_template_with_components_works() -> Result<(), WhatsappErr
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
+    let phone_number_id =
+        std::env::var("PHONE_NUMBER_ID").expect("Missing environment variable PHONE_NUMBER_ID");
     let template_name = "sample_shipping_confirmation";
     let language = "en_US";
     let parameters = Vec::from([Parameter::from_text("3")]);
     let components = Vec::from([Component::with_parameters("body", parameters)]);
     let template = Template::with_components(template_name, language, components);
     let message = Message::from_template(&to, template);
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
