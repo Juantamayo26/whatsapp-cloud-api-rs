021f53fd32ccdd523ec511500ca7a3f11c84ea8e
Juantamayo26 <juancyepest@gmail.com>
2023-01-20 01:04:48 -0500

    fixes

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -3,7 +3,7 @@ mod message;
 mod message_response;
 mod template;
 
-pub use component::{Component, Currency, DateTime, Media, Parameter};
+pub use component::{Component, Parameter};
 pub use message::{Message, Text};
 pub use message_response::{ContactResponse, CreatedMessage, MessageResponse};
 pub use template::{Language, Template};
